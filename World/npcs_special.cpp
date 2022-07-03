b006c67c24d86d3b7f7648f0bc66c78212dde105
AlterEgo <admin@projectskyfire.org>
2022-07-03 11:39:20 -0500

    Undo
    Signed-off-by: AlterEgo <admin@projectskyfire.org>

--- World/npcs_special.cpp
+++ World/npcs_special.cpp
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-//Change Test
+
 /* ScriptData
 SDName: Npcs_Special
 SD%Complete: 100
